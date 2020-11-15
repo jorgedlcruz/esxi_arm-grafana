6ab9eb62519b03d3e39509e6c7ec99bdd18618a9
Jorge de la Cruz <jorgedlcruz@gmail.com>
2020-11-15 00:11:47 +0000

    No more SSH, welcome .json

--- esxi-arm-rpi_grafana.sh
+++ esxi-arm-rpi_grafana.sh
@@ -9,8 +9,8 @@
 ##      .Notes
 ##      NAME:  esxi-arm-rpi_grafana.sh
 ##      ORIGINAL NAME: esxi-arm-rpi_grafana.sh
-##      LASTEDIT: 13/11/2020
-##      VERSION: 1.0
+##      LASTEDIT: 14/11/2020
+##      VERSION: 1.1
 ##      KEYWORDS: VMware, ESXi, ARM
    
 ##      .Link
@@ -19,26 +19,32 @@
 
 # Configurations
 ##
-# Endpoint URL for InfluxDB
-InfluxDBURL="http://YOURINFLUXSERVERIP" #Your InfluxDB Server, http://FQDN or https://FQDN if using SSL
-InfluxDBPort="8086" #Default Port
-InfluxDB="telegraf" #Default Database
-InfluxDBUser="USER" #User for Database
-InfluxDBPassword='PASSWORD' #Password for Database
 thepimonPath="/thpimon-main/pyUtil/pimon_util.py"
-jumBoxUser="YOURUSER"
-jumpBoxIP="YOURIP"
 
 ##
 # Variables from Python thepimon to InfluxDB
 ##
 thepimon_output=$(python /thpimon-main/pyUtil/pimon_util.py)
 arm_rpi_firmware=$(echo $thepimon_output | awk -F':' '{print $2}' | awk '{print $1}')
-arm_rpi_boardmodel=$(echo $thepimon_output | awk -F':' '{print $2}' | awk '{print $1}')
-arm_rpi_boardrevision=$(echo $thepimon_output | awk -F':' '{print $2}' | awk '{print $1}')
-arm_rpi_boardserial=$(echo $thepimon_output | awk -F':' '{print $2}' | awk '{print $1}')
+arm_rpi_boardmodel=$(echo $thepimon_output | awk -F'Model:' '{print $2}' | awk '{print $1}')
+arm_rpi_boardrevision=$(echo $thepimon_output | awk -F'Board Revision:' '{print $2}' | awk '{print $1}')
+arm_rpi_boardserial=$(echo $thepimon_output | awk -F'Serial:' '{print $2}' | awk '{print $1}')
 arm_rpi_boardtemp=$(echo $thepimon_output | awk -F'Temp:' '{print $2}'  | awk '{print $1}')
 arm_rpi_hostname=$(echo $HOSTNAME)
 
-echo "Writing vsphere_esxi_arm to InfluxDB"
-ssh $jumBoxUser@$jumpBoxIP "curl -i -XPOST '$InfluxDBURL:$InfluxDBPort/write?precision=s&db=$InfluxDB' -u '$InfluxDBUser:$InfluxDBPassword' --data-binary 'vsphere_esxi_arm,arm_rpi_hostname=$arm_rpi_hostname,arm_rpi_firmware=$arm_rpi_firmware,arm_rpi_boardmodel=$arm_rpi_boardmodel,arm_rpi_boardrevision=$arm_rpi_boardrevision,arm_rpi_boardserial=$arm_rpi_boardserial arm_rpi_boardtemp=$arm_rpi_boardtemp' -k --silent"
+echo "Building the JSON file"
+FILE="/usr/lib/vmware/hostd/docroot/ui/pimonarm.json"
+/bin/cat <<EOM >$FILE
+{
+    "thpimon": {
+        "title": "The simplest way to take data out of the ESXi",
+        "firmware": "$arm_rpi_firmware",
+        "boardmodel": "$arm_rpi_boardmodel",
+        "boardrevision": "$arm_rpi_boardrevision",
+        "boardserial": "$arm_rpi_boardserial",
+        "boardtemp": "$arm_rpi_boardtemp",
+        "hostname": "$arm_rpi_hostname"
+    }
+}
+EOM
+echo "All good, now you can go to https://$arm_rpi_hostname/ui/pimonarm.json and use the JSON on your favourite Monitoring system"
